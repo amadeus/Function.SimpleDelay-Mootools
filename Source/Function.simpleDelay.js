8f3bfe2c5f576dce9597aa70da4e35b0f7dd4a33
amadeus <amadeusdemarzi@gmail.com>
2010-05-27 10:38:01 -0700

    making it more moo-ified

--- Source/Function.simpleDelay.js
+++ Source/Function.simpleDelay.js
@@ -15,33 +15,26 @@ provides: [Function.simpleDelay]
 */
 
 (function(){
-	if(Function.prototype.simpleDelay) return;
-
-	Function.prototype.simpleDelay = function(delay,bind,args){
-		if(this._sDelay) this.sClear();
-		var _func = this;
-		var _args = args ? args : [];
-		var _bind = bind ? bind : window;
-
-		this._sDelay = setTimeout(_func.bind(_bind,_args),delay);
-
-		return this;
-	};
-
-	Function.prototype.simpleClear = function(){
-		if(!this._sDelay) return this;
-
-		clearTimeout(this._sDelay);
-		delete this._sDelay;
-
-		return this;
-	};
-	
-	Function.prototype.sDelay = function(delay,bind,args){
-		return this.simpleDelay(delay,bind,args);
-	};
-	
-	Function.prototype.sClear = function(){
-		return this.simpleClear();
-	};
+	if(Function.prototype.simpleDelay || Function.prototype.sDelay) return;
+	Function.implement({
+		simpleDelay:function(delay,bind,args){
+			if(this._sDelay) this.sClear();
+			var _func = this;
+			var _args = args ? args : [];
+			var _bind = bind ? bind : window;
+			this._sDelay = setTimeout(_func.bind(_bind,_args),delay);
+			return this;
+		},
+		simpleClear:function(){
+			if(!this._sDelay) return this;
+			clearTimeout(this._sDelay);
+			delete this._sDelay;
+			return this;
+		}
+	});
+	// Shortcuts
+	Function.implement({
+		sDelay:function(delay,bind,args){ return this.simpleDelay(delay,bind,args); },
+		sClear:function(){ return this.simpleClear(); }
+	});
 })();
