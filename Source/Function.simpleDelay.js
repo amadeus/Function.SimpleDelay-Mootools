1be6895344295764eeb9e167eaf8a3435157da07
Arian <arian@aryweb.nl>
2010-11-12 22:50:46 +0100

    Update Function.SimpleDelay for MooTools 1.3

--- Source/Function.simpleDelay.js
+++ Source/Function.simpleDelay.js
@@ -8,14 +8,13 @@ authors:
 - Amadeus Demarzi (http://enmassellc.com/)
 - Arian Stolwijk
 
-requires:
- core/1.2.4: '*'
-
+requires: [Core/Function]
 provides: [Function.simpleDelay]
 ...
 */
 
 Function.implement({
+
 	simpleDelay:function(delay,bind,args){
 		if(this._sTimer) this.simpleClear();
 		this._sTimer = this.delay.apply(this,arguments);
@@ -32,9 +31,10 @@ Function.implement({
 		delete this._sTimer;
 		return this;
 	}
+
+}).alias({
+	sDelay: 'simpleDelay',
+	sPeriodical: 'simplePeriodical',	
+	sClear: 'simpleClear'
 });
 
-// Shortcuts
-$each({sDelay: 'simpleDelay', sPeriodical: 'simplePeriodical', sClear: 'simpleClear'},function(oldMethod,newMethod){
-	Function.prototype[newMethod] = Function.prototype[oldMethod];
-});
