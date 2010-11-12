25fcff4f7acde2955e2e5e1c3ed4da5f174b11f8
Arian <arian@aryweb.nl>
2010-11-12 22:55:56 +0100

    Fix Code Style

--- Source/Function.simpleDelay.js
+++ Source/Function.simpleDelay.js
@@ -15,18 +15,20 @@ provides: [Function.simpleDelay]
 
 Function.implement({
 
-	simpleDelay:function(delay,bind,args){
-		if(this._sTimer) this.simpleClear();
-		this._sTimer = this.delay.apply(this,arguments);
+	simpleDelay: function(delay, bind, args){
+		if (this._sTimer) this.simpleClear();
+		this._sTimer = this.delay.call(this, delay, bind, args);
 		return this;
 	},
-	simplePeriodical:function(delay,bind,args){
-		if(this._sTimer) this.simpleClear();
-		this._sTimer = this.periodical.apply(this,arguments);
+
+	simplePeriodical: function(delay, bind, args){
+		if (this._sTimer) this.simpleClear();
+		this._sTimer = this.periodical.call(this, delay, bind, args);
 		return this;
 	},
-	simpleClear:function(){
-		if(!this._sTimer) return this;
+
+	simpleClear: function(){
+		if (!this._sTimer) return this;
 		clearTimeout(this._sTimer);
 		delete this._sTimer;
 		return this;
